ce631e5b023533f513b8c3a6ef5f77b520fc887c
KelfiBM <kelfi.services@gmail.com>
2020-05-10 17:33:06 -0400

    Order Now button added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,20 +19,22 @@ class BurgerBuilder extends Component {
       meat: 0,
     },
     totalPrice: 4,
+    purchasable: false,
   };
 
   addIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
     const updatedCount = oldCount + 1;
-    const uptdatedIngredientes = {
+    const updatedIngredients = {
       ...this.state.ingredients,
     };
-    uptdatedIngredientes[type] = updatedCount;
+    updatedIngredients[type] = updatedCount;
 
     const priceAddition = INGREDIENT_PRICES[type];
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddition;
-    this.setState({ totalPrice: newPrice, ingredients: uptdatedIngredientes });
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+    this.updatePurchaseState(updatedIngredients);
   };
 
   removeIngredientHandler = (type) => {
@@ -43,19 +45,31 @@ class BurgerBuilder extends Component {
     }
 
     const updatedCount = oldCount - 1;
-    const uptdatedIngredientes = {
+    const updatedIngredients = {
       ...this.state.ingredients,
     };
-    uptdatedIngredientes[type] = updatedCount;
+    updatedIngredients[type] = updatedCount;
     const priceReduction = INGREDIENT_PRICES[type];
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice - priceReduction;
 
     this.setState({
       totalPrice: newPrice,
-      ingredients: uptdatedIngredientes,
+      ingredients: updatedIngredients,
     });
+    this.updatePurchaseState(updatedIngredients);
   };
+
+  updatePurchaseState(ingredients) {
+    const sum = Object.keys(ingredients)
+      .map((ingredientKey) => {
+        return ingredients[ingredientKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    this.setState({ purchasable: sum > 0 });
+  }
 
   render() {
     const disabledInfo = {
@@ -74,6 +88,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           price={this.state.totalPrice}
+          purchasable={this.state.purchasable}
         />
       </Fragment>
     );
