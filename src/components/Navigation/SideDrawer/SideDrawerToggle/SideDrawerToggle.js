7f24039b284fa50b8952162559ef750018481bda
KelfiBM <kelfi.services@gmail.com>
2020-05-15 16:35:50 -0400

    Adding styling to SideDrawerToggle component

--- src/components/Navigation/SideDrawer/SideDrawerToggle/SideDrawerToggle.js
+++ src/components/Navigation/SideDrawer/SideDrawerToggle/SideDrawerToggle.js
@@ -4,7 +4,13 @@ import PropTypes from "prop-types";
 import classes from "./SideDrawerToggle.module.css";
 
 const sideDrawerToggle = (props) => {
-  return <div onClick={props.click}>Menu</div>;
+  return (
+    <div onClick={props.click} className={classes.SideDrawerToggle}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 sideDrawerToggle.propTypes = {
