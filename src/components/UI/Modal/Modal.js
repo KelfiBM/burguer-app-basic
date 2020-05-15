33511ea7c973f52f7aa0df1efb429a8e5cd49f2c
KelfiBM <kelfi.services@gmail.com>
2020-05-15 17:04:08 -0400

    Adding ShouldComponenetUpdate validation to Modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,27 +3,34 @@ import PropTypes from "prop-types";
 
 import classes from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
+import { Component } from "react";
 
-const modal = (props) => {
-  return (
-    <Fragment>
-      <Backdrop show={props.show} click={props.modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0",
-        }}
-      >
-        {props.children}
-      </div>
-    </Fragment>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-modal.propTypes = {
+  render() {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} click={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+Modal.propTypes = {
   show: PropTypes.bool.isRequired,
   modalClosed: PropTypes.func,
 };
 
-export default modal;
+export default Modal;
